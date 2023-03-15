04eff1f2356ddb548a0d5bde15d4be009afcf980
maanemann <hans.august@gmail.com>
2023-03-15 11:45:29 +0100

    Add BILLEDER! og andre ting siden sidst, tror jeg.

--- scripts/pokemons.js
+++ scripts/pokemons.js
@@ -1,25 +1,28 @@
-const pokemons = document.querySelector(".pokemons");
+const pokemons = document.querySelector("#pokemons");
 
 fetch('https://pokeapi.co/api/v2/pokemon')
     .then((response) => response.json())
     .then(data => {
-        console.log(data.results[0].name);
-        data.results.name.forEach(element => {
-            console.log(element);
+        // console.log(data)
+        // console.log(data.results[0].name);
+        data.results.forEach(pokemon => {
+            console.log(pokemon);
+            // fetch(pokemon.url)
+            //     .then((response) => response.json())
+            //     .then(data => {
+            //         let pokemonId = data.id;
+            //     })
+            //     ;
+            pokemons.innerHTML += `
+                <figure>
+                    <a href="html/details.html">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.replace('https://pokeapi.co/api/v2/pokemon/','').replace('/','')}.png" alt="This should be an image of a Pokémon">
+                        <h2>
+                            ${pokemon.name}
+                        </h2>
+                    </a>
+                </figure>
+            `
         });
-
-        // data.destinations.forEach(destination => {
-        //     places.innerHTML += `
-        //     <figure id="${destination.id}">
-        //         <img src="./img/${destination.image}" alt="" />
-        //         <div>
-        //             <i class="fa-solid fa-heart"></i>
-        //             <a href="destination.html?id=${destination.id}">
-        //                 More
-        //             </a>
-        //         </div>
-        //     </figure>
-        //     `
-        // });
     })
     ;
